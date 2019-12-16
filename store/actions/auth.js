8b9a6f4061a01b25016aabc9b8f58b2556ee95c0
jcelis13 <jannmartiecelis13@gmail.com>
2019-12-16 21:49:33 +0800

    search bar updated

--- store/actions/auth.js
+++ store/actions/auth.js
@@ -17,11 +17,12 @@ export const authStart = () => {
     };
 };
 
-export const authSuccess = (token, userProfile) => {
+export const authSuccess = (token, userProfile, searchEmployee) => {
     return {
         type: AUTH_SUCCESS,
         userData: token,
-        userProfile: userProfile
+        userProfile: userProfile,
+        searchEmployee: searchEmployee
     };
 };
 export const authFail = (error) => {
@@ -92,7 +93,7 @@ export const auth = (username, password) => {
         })
         if(response.ok){
             const res = await response.json();
-            dispatch(authSuccess(res.userData, res.userProfile))
+            dispatch(authSuccess(res.userData, res.userProfile, res.searchEmployee))
             if(typeof window !== 'undefined'){
                 window[WINDOW_USER_SCRIPT_VARIABLE] = res.userData || {} ;
             }
